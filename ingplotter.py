9a717c01cf01e4a0674db13ee479300fb87c0911
Nils Mannem <nilsmannem36@gmail.com>
2021-10-02 16:42:37 +0200

    test

--- ingplotter.py
+++ ingplotter.py
@@ -7,4 +7,15 @@ naam = input()
 if naam == 'nils':
     print('hallo baas')
 else:
-    print('hoi',naam)
+     print('hoi',naam)
+print('wat kan ik voor je doen')
+while True:
+    doen = input()
+    if doen == '':
+        print('dit is geen optie')
+        print('probeer iets anders')
+    elif doen == 'eet een blokje kaas':
+        print('chomp chomp')
+    elif doen == 'niks':
+        exit()
+   
